cdb5f87aa737365c83c0e97e88582b7e7827f72d
Geo Fyns GitHub <36727209+geofyn@users.noreply.github.com>
2025-05-06 15:07:35 +0200

    Create personer der bor i sommerhuse.sql

---
+++ ejendomsdataudtraek/personer der bor i sommerhuse.sql
@@ -0,0 +1,19 @@
+Select Fornavn,
+Efternavn,
+CPR,
+enh041LovligAnvendelse_T,
+enh042DatoForTidbegraensDisp, 
+enh048GodkendtTomBolig_T,
+enh068FlexboligTillArt_T,
+enh069FlexboligOphoersDato,
+byg.Vejnavn, byg.HusNr, 
+byg.Postnr As Postnr1,
+byg.PostDistrikt,
+byg.byg021BygningensAnvendelse_T,
+cpr.Adressedato, EnhAdr_id, enh.Bygning
+From CPR_AktivView cpr 
+
+Inner Join BBR.EnhedGeoView enh On enh.Adresse = cpr.EnhAdr_id 
+Inner Join BBR.BygningGeoView byg On byg.BygningId = enh.Bygning 
+
+Where byg.byg021BygningensAnvendelse = 510
